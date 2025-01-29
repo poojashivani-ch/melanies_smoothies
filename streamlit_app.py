a41fc4b9848c9351366935cad8c75fef843e8282
poojashivani-ch <poojas@accelerize360.com>
2025-01-29 14:20:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,16 +30,17 @@ if ingredients_list:
         ingredients_string+=fruit_chosen+' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
-    #st.write(my_insert_stmt)
-    #st.stop()
-
         time_to_insert=st.button('Submit Order')
         if time_to_insert:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+
+    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            #values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    #st.write(my_insert_stmt)
+    #st.stop()
+
+       
 
 
 
